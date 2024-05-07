c353ee01a3aa3a5a2b3b66d0434f2261057aa487
Fernando Javier Tejerina <101528363+fertejj@users.noreply.github.com>
2024-05-07 16:16:14 -0300

    Ejercicio 6 terminado

---
+++ src/ar/edu/unju/fi/ejercicio6/model/FelinoSalvaje.java
@@ -0,0 +1,18 @@
+
+package ar.edu.unju.fi.ejercicio6.model;
+
+public class FelinoSalvaje extends FelinoDomestico{
+
+	public FelinoSalvaje() {
+		
+	}
+	
+	public FelinoSalvaje(String nombre, byte edad, float peso) {
+		super(nombre, edad, peso);
+	}
+
+	@Override
+	public String toString() {
+		return "Felino Salvaje [Nombre: " + getNombre() + " - Edad: " + getEdad() + " - Peso: " + getPeso() + "kg]";
+	}
+}
